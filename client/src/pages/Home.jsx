6b48d42b74698dc482f9a1da7fc7e2750b592a9b
Max <maxi.gauthier112@gmail.com>
2026-02-21 14:22:10 +0100

    Ajout de l'init de react

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+    return (
+        <>
+
+            <section>
+                <div className="container my-4">
+                    <div>
+                        <h1>Accueil</h1>
+                        <p>Bienvenue sur Trouve ton artisan.</p>
+                    </div>
+                </div>
+
+            </section>
+
+        </>
+    );
+}
---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+    return (
+        <>
+            <section>
+                <div className="container my-4 text-center">
+                    <h1>Page non trouvée</h1>
+                </div>
+            </section>
+        </>
+    );
+}
